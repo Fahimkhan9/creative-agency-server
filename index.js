569cb1b90664424ef0ed03f2ba3ef4d590dec742
Fahimkhan9 <61534271+Fahimkhan9@users.noreply.github.com>
2020-10-15 19:06:16 +0600

    done

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-
 const express = require("express");
 const fileupload = require("express-fileupload");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const fs = require("fs-extra");
 const MongoClient = require("mongodb").MongoClient;
+require('dotenv').config({path:'./config/.env'})
 const app = express();
 const port = 5000;
 
@@ -17,18 +17,20 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-
+const uri =
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vigvf.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+//db connection
 client.connect((err) => {
   const orderscollection = client.db("creativeagency").collection("orders");
   const reviewcollection = client.db("creativeagency").collection("reviews");
   const servicescollection = client.db("creativeagency").collection("services");
   const admincollection = client.db("creativeagency").collection("admin");
 
-
+//add orders
   app.post("/addorder", (req, res) => {
     const orderinfo = req.body;
     console.log(orderinfo);
@@ -36,22 +38,22 @@ client.connect((err) => {
       console.log(result.insertedCount > 0);
     });
   });
-
+//get orders by email
   app.get("/getorders", (req, res) => {
     orderscollection
       .find({ email: req.query.email })
       .toArray((err, documents) => {
         res.send(documents);
-        // console.log(documents);
+      
       });
   });
-
+//get all reviews
   app.get("/getallreviews", (req, res) => {
     reviewcollection.find({}).toArray((err, documents) => {
       res.send(documents);
     });
   });
-
+// add  a review
   app.post("/addreview", (req, res) => {
     const reviewinfo = req.body;
     console.log(reviewinfo);
@@ -59,13 +61,13 @@ client.connect((err) => {
       console.log(result.insertedCount > 0);
     });
   });
-
+//get all orders
   app.get("/getallorders", (req, res) => {
     orderscollection.find({}).toArray((err, documents) => {
       res.send(documents);
     });
   });
-
+//add a service
   app.post("/addservice", (req, res) => {
     const file = req.files.file;
     const name = req.body.name;
@@ -102,27 +104,30 @@ client.connect((err) => {
       // return res.send({name: file.name,path:`/${file.name}`})
     });
   });
-
+//get all services
   app.get("/allservcies", (req, res) => {
     servicescollection.find({}).toArray((err, documents) => {
       res.send(documents);
     });
   });
 
-app.post('/isdoctor',(req,res) => {
-  const email = req.body.email
-admincollection.find({email: email})
-.toArray((err,documents) => {
-  res.send(documents.length > 0)
-  console.log(documents.length > 0);
-})
-})
+  app.post("/isdoctor", (req, res) => {
+    const email = req.body.email;
+    admincollection.find({ email: email }).toArray((err, documents) => {
+      res.send(documents.length > 0);
+      console.log(documents.length > 0);
+    });
+  });
 
 
+app.post("/makeadmin",(req,res) => {
+  const email = req.body
+  console.log(email);
+  admincollection.insertOne(email)
+  .then(result => console.log(result.insertedCount))
+})
 
   console.log("db connected");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+app.listen(process.env.PORT ||  port);
